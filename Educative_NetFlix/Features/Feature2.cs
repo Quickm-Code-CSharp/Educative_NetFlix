dced2706894310a772682a62a7b583a94c08210a
quickm <quickm.career@gmail.com>
2023-03-30 22:55:32 -0700

    Add Feature2 class and move feature 2 method to Feature2 class.

---
+++ Educative_NetFlix/Features/Feature2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educative_NetFlix.Features
+{
+    class Feature2
+    {
+        public static LinkedListNode MergeKSortedLists(List<LinkedListNode> lists)
+        {
+            if (lists.Count > 0)
+            {
+                LinkedListNode res = lists[0];
+
+                for (int i = 1; i < lists.Count; i++)
+                {
+                    res = Merge2SortedLists(res, lists[i]);
+                }
+
+                return res;
+            }
+            return new LinkedListNode(-1);
+        }
+
+        protected static LinkedListNode Merge2SortedLists(LinkedListNode list1, LinkedListNode list2)
+        {
+            LinkedListNode dummy = new LinkedListNode(-1);
+
+            LinkedListNode prev = dummy;
+            while (list1 != null && list2 != null)
+            {
+                if (list1.data <= list2.data)
+                {
+                    prev.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    prev.next = list2;
+                    list2 = list2.next;
+                }
+                prev = prev.next;
+            }
+
+            prev.next = list1 ?? list2;
+
+            return dummy.next;
+        }
+
+    }
+}
--- Educative_NetFlix/Features/FeatureInvoker.cs
+++ Educative_NetFlix/Features/FeatureInvoker.cs
@@ -11,49 +11,6 @@ namespace Educative_NetFlix.Features
     {
         public static List<List<string>> Feature1(string[] strs) => Features.Feature1.GroupTitles(strs);
 
-        public static LinkedListNode     Feature2(List<LinkedListNode> lists) => MergeKSortedLists(lists);
-
-        protected static LinkedListNode MergeKSortedLists(List<LinkedListNode> lists)
-        {
-            if (lists.Count > 0)
-            {
-                LinkedListNode res = lists[0];
-
-                for (int i = 1; i < lists.Count; i++)
-                    res = Merge2SortedLists(res, lists[i]);
-
-                return res;
-            }
-            return new LinkedListNode(-1);
-        }
-
-        protected static LinkedListNode Merge2SortedLists(LinkedListNode list1, LinkedListNode list2)
-        {
-            LinkedListNode dummy = new LinkedListNode(-1);
-
-            LinkedListNode prev = dummy;
-            while (list1 != null && list2 != null)
-            {
-                if (list1.data <= list2.data)
-                {
-                    prev.next = list1;
-                    list1 = list1.next;
-                }
-                else
-                {
-                    prev.next = list2;
-                    list2 = list2.next;
-                }
-                prev = prev.next;
-            }
-
-            if (list1 == null)
-                prev.next = list2;
-            else
-                prev.next = list1;
-
-            return dummy.next;
-        }
-
+        public static LinkedListNode     Feature2(List<LinkedListNode> lists) => Features.Feature2.MergeKSortedLists(lists);
     }
 }
