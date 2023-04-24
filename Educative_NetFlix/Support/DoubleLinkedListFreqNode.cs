977bb9565396906c0a14f7102bb36bee75ccb497
quickm <quickm.career@gmail.com>
2023-04-23 17:07:13 -0700

    Add IDoubleLinkedListNode interface

---
+++ Educative_NetFlix/Support/DoubleLinkedListFreqNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educative_NetFlix.Support
+{
+    class DoubleLinkedListFreqNode : DoubleLinkedListNode
+    {
+        int freq;
+
+        public DoubleLinkedListFreqNode(int data) : base(data)
+        {
+        }
+
+        public DoubleLinkedListFreqNode(int key, int data) : base(key, data)
+        {
+        }
+
+        public DoubleLinkedListFreqNode(int key, int data, int freq) : base(key, data)
+        {
+            this.freq = freq;
+        }
+
+        public DoubleLinkedListFreqNode(int data, DoubleLinkedListNode next) : base(data, next)
+        {
+        }
+
+        public DoubleLinkedListFreqNode(int data, DoubleLinkedListNode next, DoubleLinkedListNode prev) : base(data, next, prev)
+        {
+        }
+    }
+}
